780c77997741f2d26fa03d3cc4f2c7736f54dfb6
Enzo Couto <enzo@gec.inatel.br>
2025-11-29 12:38:25 -0300

    Deploy

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ app = FastAPI(
 origins = [
     "http://localhost:5173", 
     "http://localhost:3000", 
+    "https://smart-grow-4enawmc3i-gustavos-projects-6aab325e.vercel.app"
     "*"                      
 ]
 
