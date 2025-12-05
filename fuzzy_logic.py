b663a2d7b91c985e7dddcf464ad2d21c5aa95b12
Enzo Couto <enzo@gec.inatel.br>
2025-12-05 16:51:09 -0300

    Código final

--- fuzzy_logic.py
+++ fuzzy_logic.py
@@ -2,38 +2,35 @@ import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 
-# --- Variáveis de Entrada ---
+#  Variáveis de Entrada 
 temperatura = ctrl.Antecedent(np.arange(0, 51, 1), 'temperatura')
 umidade = ctrl.Antecedent(np.arange(0, 101, 1), 'umidade')
 
-# --- Variáveis de Saída ---
+#  Variáveis de Saída 
 irrigacao = ctrl.Consequent(np.arange(0, 101, 1), 'irrigacao')
 ventilacao = ctrl.Consequent(np.arange(0, 101, 1), 'ventilacao')
 
-# --- Funções de Pertinência (Temperatura) ---
+#  Funções de Pertinência (Temperatura) 
 temperatura['fria'] = fuzz.trimf(temperatura.universe, [0, 0, 20])
 temperatura['agradavel'] = fuzz.trimf(temperatura.universe, [15, 22, 30])
 temperatura['quente'] = fuzz.trimf(temperatura.universe, [25, 50, 50])
 
-# --- Funções de Pertinência (Umidade) ---
+#  Funções de Pertinência (Umidade) 
 umidade['seca'] = fuzz.trimf(umidade.universe, [0, 0, 40])
 umidade['ideal'] = fuzz.trimf(umidade.universe, [30, 50, 70])
 umidade['umida'] = fuzz.trimf(umidade.universe, [60, 100, 100])
 
-# --- Funções de Pertinência (Saídas - Irrigação) ---
+#  Funções de Pertinência (Saídas - Irrigação) 
 irrigacao['baixa'] = fuzz.trimf(irrigacao.universe, [0, 0, 50])
 irrigacao['media'] = fuzz.trimf(irrigacao.universe, [25, 50, 75])
 irrigacao['alta'] = fuzz.trimf(irrigacao.universe, [50, 100, 100])
 
-# --- Funções de Pertinência (Saídas - Ventilação) ---
+#  Funções de Pertinência (Saídas - Ventilação) 
 ventilacao['baixa'] = fuzz.trimf(ventilacao.universe, [0, 0, 50])
 ventilacao['media'] = fuzz.trimf(ventilacao.universe, [25, 50, 75])
 ventilacao['alta'] = fuzz.trimf(ventilacao.universe, [50, 100, 100])
 
-# =============================================================================
-# SISTEMA 1: IRRIGAÇÃO
-# =============================================================================
-# Regras baseadas em Temperatura e Umidade
+
 regra_irr_1 = ctrl.Rule(temperatura['quente'] & umidade['seca'], irrigacao['alta'])
 regra_irr_2 = ctrl.Rule(temperatura['agradavel'] & umidade['seca'], irrigacao['media'])
 regra_irr_3 = ctrl.Rule(umidade['ideal'], irrigacao['baixa'])
@@ -48,10 +45,7 @@ def calcular_nivel_irrigacao(temp_atual, umidade_atual):
     simulador_irrigacao.compute()
     return simulador_irrigacao.output['irrigacao']
 
-# =============================================================================
-# SISTEMA 2: VENTILAÇÃO
-# =============================================================================
-# Regras baseadas apenas em Temperatura
+
 regra_ven_1 = ctrl.Rule(temperatura['fria'], ventilacao['baixa'])
 regra_ven_2 = ctrl.Rule(temperatura['agradavel'], ventilacao['baixa']) 
 regra_ven_3 = ctrl.Rule(temperatura['quente'], ventilacao['alta'])
